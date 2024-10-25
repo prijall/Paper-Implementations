cc66b55408efdc7d16f313c51c0c46c02613e66f
prijall <khadkaprijal@gmail.com>
2024-10-25 20:58:37 +0545

    config added

--- YOLO/config.py
+++ YOLO/config.py
@@ -3,6 +3,16 @@
 import os
 import torchvision.transforms as T
 
+
+DATA_PATH='data'
+CLASSES_PATH=os.path.join(DATA_PATH, 'classes.json')
+
+BATCH_SIZE = 64
+EPOCHS = 135
+WARMUP_EPOCHS = 0
+LEARNING_RATE = 1E-4
+
+EPSILON=1E-4
 IMAGE_SIZE=(448, 448)
 
 # the predictions are encoded as S x S x (B * 5 + C):
